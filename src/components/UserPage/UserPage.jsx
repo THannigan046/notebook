804cd7f239481b1f687aab58fd3ef16e842a73a6
Toivo Hannigan <thannigan046@gmail.com>
2023-08-15 15:48:17 -0500

    tightened up front end

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,34 +1,48 @@
-import {useState} from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import { useSelector, useDispatch } from 'react-redux';
-import {Typography, Modal, TextField, Backdrop, Checkbox, Slider, Button, TableContainer, Box, Paper} from '@mui/material'
-import { DataGrid } from '@mui/x-data-grid';
-import axios from 'axios';
+import { useState } from "react";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  Typography,
+  Modal,
+  TextField,
+  Backdrop,
+  Checkbox,
+  Slider,
+  Button,
+  TableContainer,
+  Box,
+  Paper,
+  InputLabel,
+  FormControl,
+  NativeSelect,
+} from "@mui/material";
+import { NumericFormat } from "react-number-format";
+import { DataGrid } from "@mui/x-data-grid";
+import axios from "axios";
 
 function UserPage() {
-
+  const MAX_LIMIT = 7;
+  const MIN_LIMIT = 1;
   const dispatch = useDispatch();
-  const [petName, setPetName] = useState('');
+  const [petName, setPetName] = useState("");
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (daysPerWeek > 0 || daysPerWeek < 7) {
-      axios.post('/api/user/tasks', {
-        taskName: taskName, daysPerWeek: daysPerWeek
-      } )
+    axios.post("/api/user/tasks", {
+      taskName: taskName,
+      daysPerWeek: daysPerWeek,
+    });
 
-      handleClose();  
-    }
-    else (alert('Please enter a valid number of days per week'))
+    handleClose();
     /* dispatch({
       type : 'ADD_PET',
       payload: {
         name : petName
       }
     }) */
-  }
+  };
 
   const [open, setOpen] = useState(false);
-  const [taskName, setTaskName] = useState('');
+  const [taskName, setTaskName] = useState("");
   const [daysPerWeek, setDaysPerWeek] = useState(0);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -46,47 +60,101 @@ function UserPage() {
     { id: 1, lastName: 'Snow', firstName: 'Jon'},
   ] */
 
-
   const user = useSelector((store) => store.user);
   return (
     <>
-    <Typography variant="h2" component="h2" align='center'>
-      Welcome {user.username}
-    </Typography>
-    {/* rename to weeks.jsx */}
-    {/* Back button to return to months page */}
-    {/* Make a task */}
-    {/* click a button that opens a task modal */}  
-    {/* datagrid of tasks (crud stuff) with checkboxes that all feeds to database*/}
-    <Box display='flex' justifyContent='center' width='100%'>
-      <Button onClick={handleOpen}>Add Task</Button>
-    </Box>
+      <Typography variant="h2" component="h2" align="center">
+        Welcome {user.username}
+      </Typography>
+      {/* rename to weeks.jsx */}
+      {/* Back button to return to months page */}
+      {/* Make a task */}
+      {/* click a button that opens a task modal */}
+      {/* datagrid of tasks (crud stuff) with checkboxes that all feeds to database*/}
+      <Box display="flex" justifyContent="center" width="100%">
+        <Button onClick={handleOpen}>Add Task</Button>
+      </Box>
 
-      <Modal closeAfterTransition
+      <Modal
+        closeAfterTransition
         BackdropComponent={Backdrop}
-        BackdropProps={{ onClick: handleClose }} open={open} onClose={handleClose}> 
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '16px', justifyContent:'center', height: '100%', }}>
-        {/* Row 1: Text */}
-        <Paper sx={{ width: '100%', padding: '16px', marginBottom: '8px' }}>
-          {/* <Typography variant="body1">Row 1: Text Input</Typography> */}
-          <TextField fullWidth label="Task Name" value={taskName} onChange={e => setTaskName(e.target.value)} variant="outlined" />
-        </Paper>
-
-        {/* Row 2: Number */}
-        <Paper sx={{ width: '100%', padding: '16px', marginBottom: '8px' }}>
-          {/* <Typography variant="body1">Row 2: Number Input</Typography> */}
-          <TextField  value={daysPerWeek} onChange={e => setDaysPerWeek(e.target.value)} fullWidth label="Days Per Week" variant="outlined" type="number" />
-        </Paper>
-
-          <Paper sx={{ width: '100%', padding: '16px', marginBottom: '8px' }}>
-            <Box display='flex' justifyContent='center' width='100%'>
-              <Button onClick={handleSubmit} variant="contained">Add Task</Button>
-            </Box>
+        BackdropProps={{ onClick: handleClose }}
+        open={open}
+        onClose={handleClose}
+      >
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            padding: "16px",
+            justifyContent: "center",
+            height: "100%",
+          }}
+        >
+          {/* Row 1: Text */}
+          <Paper sx={{ width: "100%", padding: "16px", marginBottom: "8px" }}>
+            {/* <Typography variant="body1">Row 1: Text Input</Typography> */}
+            <TextField
+              fullWidth
+              label="Task Name"
+              value={taskName}
+              required
+              onChange={(e) => setTaskName(e.target.value)}
+              variant="outlined"
+            />
           </Paper>
 
-      </Box>
-    </Modal>
+          {/* Row 2: Number */}
+          <Paper sx={{ width: "100%", padding: "16px", marginBottom: "8px" }}>
+            {/* <Typography variant="body1">Row 2: Number Input</Typography> */}
 
+            {/* TODO: limit days per week between 1 and 7 */}
+            {/* <TextField
+              value={daysPerWeek}
+              inputProps={{ inputMode: "numeric", pattern: "[1-7]*" }}
+              onChange={(e) => setDaysPerWeek(e.target.value)}
+              fullWidth
+              label="Days Per Week"
+              variant="outlined"
+              type="number"
+            /> */}
+            <FormControl fullWidth>
+              <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                Days Per Week
+              </InputLabel>
+              <NativeSelect
+                value={daysPerWeek}
+                onChange={(e) => setDaysPerWeek(e.target.value)}
+                defaultValue={1}
+                required
+                inputProps={{
+                  name: "daysPerWeek",
+                  id: "uncontrolled-native",
+                }}
+              >
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+                <option value={6}>6</option>
+                <option value={7}>7</option>
+              </NativeSelect>
+            </FormControl>
+          </Paper>
+
+          <Paper sx={{ width: "100%", padding: "16px", marginBottom: "8px" }}>
+            <Box display="flex" justifyContent="center" width="100%">
+              <Button type="submit" variant="contained">
+                Add Task
+              </Button>
+            </Box>
+          </Paper>
+        </Box>
+      </Modal>
     </>
   );
 }
